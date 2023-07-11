2c7401720f94453b5eefb26ec59251b4c29c9be3
Saja HZ <herezsaja2020@gmail.com>
2023-07-11 07:59:37 +0300

    commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# sensors
+
+any device has built-in sensors to measure orientation, motion or other environmental conditions with a very high accuracy. To monitor the three-dimensional data the device
+ you can use the official sensors_plus package.
+
+Flutter plugin to access the accelerometer, gyroscope, and magnetometer sensors.
+
+Add `sensors_plus` as a dependency in your pubspec.yaml file.
+
+This will expose such classes of sensor events through a set of streams:
+
+- `UserAccelerometerEvent` describes the acceleration of the device, in m/$s^2$ If the device is still, or is moving along a straight line at constant speed, the reported acceleration is zero. If the device is moving e.g. towards north and its speed is increasing, the reported acceleration is towards north; if it is slowing down, the reported acceleration is towards south; if it is turning right, the reported acceleration is towards east. The data of this stream is obtained by filtering out the effect of gravity from `AccelerometerEvent`.
+
+- `AccelerometerEvent` describes the acceleration of the device, in m/$s^2$, including the effects of gravity. Unlike `UserAccelerometerEvent`, this stream reports raw data from the accelerometer (physical sensor embedded in the mobile device) without any post-processing. The accelerometer is unable to distinguish between the effect of an accelerated movement of the device and the effect of the surrounding gravitational field. This means that, at the surface of Earth, even if the device is completely still, the reading of `AccelerometerEvent` is an acceleration of intensity 9.8 directed upwards (the opposite of the graviational acceleration). This can be used to infer information about the position of the device (horizontal/vertical/tilted). `AccelerometerEvent` reports zero acceleration if the device is free falling.
+
+- `GyroscopeEvent` describes the rotation of the device.
+- `MagnetometerEvent` describes the ambient magnetic field surrounding the device. A compass is an example usage of this data.
+
+These events are exposed through a `BroadcastStream`: `accelerometerEvents`, `userAccelerometerEvents`, `gyroscopeEvents`, and `magnetometerEvents`, respectively.
+
+<https://pub.dev/packages/sensors_plus>
+<https://www.youtube.com/watch?v=Fq5zNPJufD0>
---
+++ lib/main.dart
@@ -0,0 +1,135 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:sensors_plus/sensors_plus.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  List<double>? _accelerometerValues;
+  List<double>? _userAccelerometerValues;
+  List<double>? _gyroscopeValues;
+  final _streamSubscriptions = <StreamSubscription<dynamic>>[];
+
+  @override
+  Widget build(BuildContext context) {
+    final accelerometer =
+        _accelerometerValues?.map((double v) => v.toStringAsFixed(1)).toList();
+    final gyroscope =
+        _gyroscopeValues?.map((double v) => v.toStringAsFixed(1)).toList();
+    final userAccelerometer = _userAccelerometerValues
+        ?.map((double v) => v.toStringAsFixed(1))
+        .toList();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sensor Example'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text('Accelerometer: $accelerometer'),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text('UserAccelerometer: $userAccelerometer'),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text('Gyroscope: $gyroscope'),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    for (final subscription in _streamSubscriptions) {
+      subscription.cancel();
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _streamSubscriptions.add(
+      accelerometerEvents.listen(
+        (AccelerometerEvent event) {
+          setState(() {
+            _accelerometerValues = <double>[event.x, event.y, event.z];
+          });
+         print('accelerometerEvents >> X: ${event.x}, y: ${event.y} ,z: ${event.z}');
+        },
+      ),
+    );
+    _streamSubscriptions.add(
+      gyroscopeEvents.listen(
+        (GyroscopeEvent event) {
+          setState(() {
+            _gyroscopeValues = <double>[event.x, event.y, event.z];
+          });
+          print(
+              'gyroscopeEvents >> X: ${event.x}, y: ${event.y} ,z: ${event.z}');
+        },
+      ),
+    );
+    _streamSubscriptions.add(
+      userAccelerometerEvents.listen(
+        (UserAccelerometerEvent event) {
+          setState(() {
+            _userAccelerometerValues = <double>[event.x, event.y, event.z];
+          });
+          print(
+              'userAccelerometerEvents >> X: ${event.x}, y: ${event.y} ,z: ${event.z}');
+        },
+      ),
+    );
+  }
+}
